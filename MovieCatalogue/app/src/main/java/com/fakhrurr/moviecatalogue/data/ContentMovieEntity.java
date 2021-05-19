03e9e36deeff72f779ce83be0011bee9626e0a5d
FakhrurR <fakrijal@gmail.com>
2021-05-19 21:22:22 +0700

    update

---
+++ MovieCatalogue/app/src/main/java/com/fakhrurr/moviecatalogue/data/ContentMovieEntity.java
@@ -0,0 +1,17 @@
+package com.fakhrurr.moviecatalogue.data;
+
+public class ContentMovieEntity {
+    private String mContent;
+
+    public ContentMovieEntity(String content) {
+        this.mContent = content;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String mContent) {
+        this.mContent = mContent;
+    }
+}
--- MovieCatalogue/app/src/main/java/com/fakhrurr/moviecatalogue/utils/DummyData.java
+++ MovieCatalogue/app/src/main/java/com/fakhrurr/moviecatalogue/utils/DummyData.java
@@ -1,4 +1,38 @@
 package com.fakhrurr.moviecatalogue.utils;
 
+import com.fakhrurr.moviecatalogue.R;
+import com.fakhrurr.moviecatalogue.data.MovieEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DummyData {
+    private static Helpers helpers;
+
+    public static List<MovieEntity> generateDummyMovie() {
+        ArrayList<MovieEntity> movieEntityArrayList = new ArrayList<>();
+
+        movieEntityArrayList.add(new MovieEntity(
+                "1",
+                "Alita",
+                "data movie",
+                "Senin, 20 April 2020",
+                "Rijhal",
+                helpers.getURLForResource(R.drawable.poster_alita)));
+        return movieEntityArrayList;
+    }
+
+    public static List<MovieEntity> generateDummyTVShow() {
+        ArrayList<MovieEntity> tvShowArrayList = new ArrayList<>();
+
+        tvShowArrayList.add(new MovieEntity(
+                "1",
+                "Alita",
+                "data movie",
+                "Senin, 20 April 2020",
+                "Rijhal",
+                helpers.getURLForResource(R.drawable.poster_alita)));
+
+        return tvShowArrayList;
+    }
 }
