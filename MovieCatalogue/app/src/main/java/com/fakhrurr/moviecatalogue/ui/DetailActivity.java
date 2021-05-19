f4b84f4b0ffe97f640505beb1dcde6f32e79e963
FakhrurR <fakrijal@gmail.com>
2021-05-19 08:10:24 +0700

    add detail and item movie

---
+++ MovieCatalogue/app/src/main/java/com/fakhrurr/moviecatalogue/ui/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.fakhrurr.moviecatalogue.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fakhrurr.moviecatalogue.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ MovieCatalogue/app/src/main/java/com/fakhrurr/moviecatalogue/utils/DummyData.java
@@ -0,0 +1,4 @@
+package com.fakhrurr.moviecatalogue.utils;
+
+public class DummyData {
+}
